8d5a338c09d3e1b7d277833840196472f4962a4f
Vasiliy <catanfa@yandex.ru>
2019-10-01 00:21:34 +0200

    switch to php-ews

--- README.md
+++ README.md
@@ -1,17 +1,7 @@
-## required steps
-* register Outlook app https://docs.microsoft.com/en-us/previous-versions/office/office-365-api/api/version-2.0/use-outlook-rest-api#register-and-authenticate-your-app
-* configure _Authentication_ for the app as type = _Web_, url = _http://localhost:8080/sync.php_
-* configure following API permissions for the app:
-    - Calendars.ReadWrite
-    - Calendars.ReadWrite.Shared
-    - Tasks.ReadWrite.Shared
-    - User.Read
-    - offline_access
-    - openid
- * start local web server
- ```bash
-php -S localhost:8080
+## How To Start
+* create `.env` file in the root
+```bash
+copy .env.example .env
+nano .env
 ```
-* go to [localhost:8080](http://localhost:8080]), authenticate
-* save the token to .env variable
 * finally, put the script to be run every day. It will export all vacations to your Outlook Calendar. 
--- sync.php
+++ sync.php
@@ -1,47 +1,11 @@
 <?php declare(strict_types=1);
 
-use Outlook\Authorizer\Authenticator;
-use Outlook\Authorizer\Session;
-use Outlook\Authorizer\Token;
-use Outlook\Calendars\CalendarAuthorizer;
-use Outlook\Calendars\CalendarManager;
+use Symfony\Component\Dotenv\Dotenv;
 
 require_once 'vendor/autoload.php';
 
-$redirectUri = 'https://hurma.tv/azure/sync.php';
-
-$sessionManager = new Session();
-$authenticator  = new Authenticator(getenv(APP_ID), getenv(APP_PASSWORD), $redirectUri);
-$authenticator->setSessionManager($sessionManager);
-
-// this script has to be
-$token = $authenticator->getToken();
-
-if (!$token)
-{
-	//if $token is not available then we get login url
-	$url = $authenticator->getLoginUrl($scopes = [], $redirectUri);
-	echo 'no token, login url = ' . "<a href='$url'>login</a>";
-	exit;
-}
-// if $token is available it is instance of Outlook\Authorizer\Token
-echo "got token  \n"; //. var_dump($token);
-
-$calenderAuthorizer = new CalendarAuthorizer($authenticator, $sessionManager);
-
-$calendarToken = $calenderAuthorizer->isAuthenticated();
-
-if (!$calendarToken)
-{
-	$url = $calenderAuthorizer->getLoginUrl($redirectUri, []);
-	echo "need to get calendar token: " . "<a href='$url'>login</a>";
-	exit;
-}
-
-$calendarManager = new CalendarManager($token);
-
-var_dump($calendarManager->getAllCalendars());
-
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__.'/.env');
 
 
 
